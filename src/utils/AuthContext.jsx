d4c155dc82ec4ab32b39b14b9c5973d9abcacaca
Rashi <rashi1704baranwal@gmail.com>
2024-04-25 16:38:10 +0530

    Auth completed using Appwrite

---
+++ src/utils/AuthContext.jsx
@@ -0,0 +1,69 @@
+import { useContext, useState, useEffect, createContext } from "react";
+import { account } from "../appwriteConfig";
+const AuthContext = createContext();
+
+export const AuthProvider = ({children}) => {
+  const [loading, setLoading] = useState(true);
+  const [user, setuser] = useState(false)
+  
+  useEffect(() => {
+    checkUserStatus()
+    
+  }, [])
+  
+    const loginUser = async (userInfo) => {
+    try{
+      setLoading(true)
+
+      let response = await account.createEmailPasswordSession(userInfo.email, userInfo.password)
+      let accountDetails = await account.get()
+      console.log('accountDetails: ',accountDetails)
+      setuser(accountDetails)
+    //   console.log(response);
+      setLoading(false)
+    }catch(error){
+      console.error(error);
+      window.alert("Invalid credentials"); // alert the user
+      history.push("/login"); // redirect to login page
+    }
+  }
+
+
+  const logoutUser = ()=>{
+    account.deleteSession('current')
+    setuser(null)
+  }
+
+  const registerUser = ()=>{}
+
+  const checkUserStatus = async ()=>{
+
+    try {
+        let accountDetails = await account.get()
+        setuser(accountDetails)
+    } catch (error) {
+        console.error(error)
+    }
+
+    setLoading(false)
+  }
+
+
+
+  const contextData = {
+    user,
+    loginUser,
+    logoutUser,
+    registerUser
+  }
+
+  return (
+    <AuthContext.Provider value = {contextData}>
+      {loading ? <p>Loading...</p> : children}
+    </AuthContext.Provider>
+  )
+
+}
+
+export  const useAuth = ()=> {return useContext(AuthContext)}
+export default AuthContext
