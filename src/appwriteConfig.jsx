e47ef42dc701cf5e1383da3595d16270b0b7552c
Leonardo Fernandes <leonardoschoolwork@gmail.com>
2024-04-28 23:24:27 +0530

    Update Appwrite project ID in appwriteConfig.jsx

--- src/appwriteConfig.jsx
+++ src/appwriteConfig.jsx
@@ -4,12 +4,7 @@ const client = new Client();
 
 client
   .setEndpoint("https://cloud.appwrite.io/v1")
-  .setProject("6627978b0e3308c33a1c");
-
-// This is the Project Id with dummy Questions and Answers
-// client
-//   .setEndpoint("https://cloud.appwrite.io/v1")
-//   .setProject("662a61da6824b9c5f45a");
+  .setProject("662a61da6824b9c5f45a");
 
 export const account = new Account(client);
 export default client;
